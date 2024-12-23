6afac363d0a6bdab7603fb4b74dbefd0c631724e
Florent DELANNOY <delannoy.florent@gmail.com>
2024-12-22 21:20:53 -0500

    updates and cleanup

--- src/main/java/com/project/urlshortener/configuration/properties/UrlShortenerProperties.java
+++ src/main/java/com/project/urlshortener/configuration/properties/UrlShortenerProperties.java
@@ -1,14 +1,19 @@
 package com.project.urlshortener.configuration.properties;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 /**
  * Access to some of the application parameters (application.yml).
  * @param baseUrl (String) Base of the short url. This is the base for the short url. Only the url token is missing.
- * @param token (Token) Tokem related parameters.
+ * @param token (Token) Token related parameters.
  */
 @ConfigurationProperties("url-shortener")
-public record UrlShortenerProperties (String baseUrl, Token token) {
+@Validated
+public record UrlShortenerProperties (@NotBlank String baseUrl, @NotNull Token token) {
 
 	/**
 	 *
@@ -16,6 +21,6 @@ public record UrlShortenerProperties (String baseUrl, Token token) {
 	 * @param maxAttempts (int) Maximum number of attempts to create an url token.
 	 * @param characters (String) Possible characters for an url token. These are all the available characters that can be used to create a short url token.
 	 */
-	public record Token(Integer length, Integer maxAttempts, String characters) {}
+	public record Token(@NotNull @Positive Integer length, @NotNull @Positive Integer maxAttempts, @NotBlank String characters) {}
 
 }
